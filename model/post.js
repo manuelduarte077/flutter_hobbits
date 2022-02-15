4097f4aad1e3ee07040c0cc743e2f6da4de36a56
Manuel Duarte <carlosd99m@gmail.com>
2022-02-14 23:42:19 -0600

    chore(api): refactor code

--- model/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const MSchema = mongoose.Schema;
-
-const postSchema = MSchema({
-  comment: String,
-  userId: String,
-});
-
-module.exports = mongoose.model("Post", postSchema);
--- model/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const MSchema = mongoose.Schema;
-
-const userSchema = MSchema({
-  name: String,
-  age: Number,
-  profession: String,
-});
-module.exports = mongoose.model("User", userSchema);
