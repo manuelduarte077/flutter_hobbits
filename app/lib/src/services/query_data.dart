234a1a80de82754024336b7433466ff115d90b34
Manuel Duarte <carlosd99m@gmail.com>
2022-03-06 13:58:05 -0600

    refactor code

--- app/lib/src/services/query_data.dart
+++ app/lib/src/services/query_data.dart
@@ -1,17 +1,17 @@
 class QueryData {
   String insertPost() {
-    return """
+    return '''
       mutation CreatePost(\$comment: String!, \$userId: String!) {
         CreatePost(comment: \$comment, userId: \$userId){
           id
           comment
         }   
       }
-    """;
+    ''';
   }
 
   static insertUser() {
-    return """ 
+    return ''' 
       mutation CreateUserMutation(\$name: String!, \$age: Int!, \$profession: String!){
         createUser(name: \$name, age: \$age, profession: \$profession) {
             id
@@ -20,31 +20,31 @@ class QueryData {
             age
         }
       }
-    """;
+    ''';
   }
 
   String insertHobby() {
-    return """
+    return '''
       mutation CreateHobby(\$title: String!, \$description: String!, \$userId: String!) {
         CreateHobby(title: \$title, description: \$description, userId: \$userId){
           id
           title
         }   
       }
-    """;
+    ''';
   }
 
   static String updateUser() {
-    return """
+    return '''
       mutation UpdateUser(\$id: String!, \$name: String!, \$profession: String!, \$age: Int!) {
         UpdateUser(id: \$id, name: \$name, profession: \$profession, age: \$age){
           name
         }   
       }
-    """;
+    ''';
   }
 
-  final String query = """
+  final String query = '''
     query {
       users{
         name
@@ -65,35 +65,35 @@ class QueryData {
     
     } 
  
-  """;
+  ''';
 
   String removeUser() {
-    return """
+    return '''
       mutation RemoveUser(\$id: String!) {
       RemoveUser(id: \$id){
           name
         }   
       }
-    """;
+    ''';
   }
 
   String removePosts() {
-    return """ 
+    return ''' 
       mutation RemovePosts(\$ids: [String]) {
         RemovePosts(ids: \$ids){
           
         }   
       }
-    """;
+    ''';
   }
 
   String removeHobbies() {
-    return """
+    return '''
       mutation RemoveHobbies(\$ids: [String]) {
         RemoveHobbies(ids: \$ids){
         
         }   
       }
-     """;
+     ''';
   }
 }
